2cc8ca9f8f18f873c31a5dad60d7f1f7ed223e48
hung279 <hungnher279@gmail.com>
2023-05-29 00:08:46 +0700

    init project, set up server and router, connect db

---
+++ backend/server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const connectDB = require("./config/db");
+const dotenv = require("dotenv");
+const userRoutes = require("./routes/userRoutes");
+const chatRoutes = require("./routes/chatRoutes");
+const messageRoutes = require("./routes/messageRoutes");
+
+dotenv.config();
+connectDB();
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Running!");
+});
+
+app.use("/api/user", userRoutes);
+app.use("/api/chat", chatRoutes);
+app.use("/api/message", messageRoutes);
+
+
+const PORT = process.env.PORT;
+
+const server = app.listen(
+  PORT,
+  console.log(`Server running on PORT ${PORT}...`.yellow.bold)
+);
