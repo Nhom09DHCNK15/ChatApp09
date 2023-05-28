fe52d7c8ce0ba8c7b45fd395ad519617b3d08025
hung279 <hungnher279@gmail.com>
2023-05-29 00:16:11 +0700

    Create Model, API get all users

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,5 +1,10 @@
 const express = require("express");
+const {
+  allUsers
+} = require("../controllers/userControllers");
 
 const router = express.Router();
+
+router.route("/").get(allUsers);
 
 module.exports = router;
