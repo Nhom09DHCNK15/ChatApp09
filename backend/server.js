0dc797ae4c2e0b39cf4e9ac730228040b7a7b5c0
hung279 <hungnher279@gmail.com>
2023-06-03 14:36:19 +0700

    hanlde message array received

--- backend/server.js
+++ backend/server.js
@@ -68,15 +68,23 @@ io.on("connection", (socket) => {
   socket.on("typing", (room) => socket.in(room).emit("typing"));
   socket.on("stop typing", (room) => socket.in(room).emit("stop typing"));
 
-  socket.on("new message", (newMessageRecieved) => {
-    var chat = newMessageRecieved.chat;
+  socket.on("new message", (newMessageReceived) => {
+    let chat;
+    let sender;
+    if (Array.isArray(newMessageReceived)) {
+      chat = newMessageReceived[0].chat;
+      sender = newMessageReceived[0].sender._id;
+    } else {
+      chat = newMessageReceived.chat;
+      sender = newMessageReceived.sender._id;
+    }
 
     if (!chat.users) return console.log("chat.users not defined");
 
     chat.users.forEach((user) => {
-      if (user._id == newMessageRecieved.sender._id) return;
+      if (user._id == sender) return;
 
-      socket.in(user._id).emit("message recieved", newMessageRecieved);
+      socket.in(user._id).emit("message recieved", newMessageReceived);
     });
   });
 
